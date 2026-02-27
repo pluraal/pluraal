615cfb8268d8ec4997c7636fab453078e26d4ea0
Attila Mihaly <Attila.Mihaly@MorganStanley.com>
2026-02-27 21:22:11 +0100

    Clean up everything.

--- main.js
+++
@@ -1,7 +0,0 @@
-// Import the compiled Elm app
-import ScopeViewer from './src/ScopeViewer.elm';
-
-// Initialize the Elm app
-const app = ScopeViewer.init({
-  node: document.getElementById('scope-viewer-app'),
-});
